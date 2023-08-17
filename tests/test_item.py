0dd62d873d0da8c3e6f20c273b1a6467af1e2989
Yana <yanashat198@gmail.com>
2023-08-17 11:40:42 +0700

    add main, tests, item

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,8 +10,10 @@ def test_data():
 
 
 def test_calculate_total_price(test_data):
+    """"Тестируем метод, который считает общую сумму"""
     assert test_data.calculate_total_price() == 200000
 
 
 def test_apply_discount(test_data):
+    """Тестируем метод, который применяет скидку"""
     assert test_data.apply_discount() == 8000
